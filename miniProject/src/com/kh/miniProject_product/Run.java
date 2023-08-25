8524ebe094aca46bfc40ca9c4901da7c65ac0c36
GIYUN9 <giyoon01292@naver.com>
2023-08-25 17:38:25 +0900

    Run add . menu

--- miniProject/src/com/kh/miniProject_product/Run.java
+++ miniProject/src/com/kh/miniProject_product/Run.java
@@ -1,8 +1,36 @@
 package com.kh.miniProject_product;
 
+import java.util.Scanner;
+
 public class Run {
 	public static void main(String[] args) {
 		ProductMenu pm = new ProductMenu();
+		Scanner sc = new Scanner(System.in);
+		
+		while(true) {
+			System.out.println("======= Main =======");
+			System.out.println("1. 고객 메뉴");
+			System.out.println("2. 관리자 메뉴");
+			System.out.println("9. 프로그램 종료");
+			System.out.println("번호를 선택해주세요 : ");
+			int mainNum = sc.nextInt();
+			
+			switch(mainNum) {
+				case 1:
+					// 1. 고객 메뉴
+					break;
+				case 2:
+					// 2. 관리자 로그인
+					break;
+				case 9:
+					// 9. 프로그램 종료
+					System.out.println("[프로그램을 종료합니다.]");
+					return;
+				default:
+					System.out.println("잘못된 번호입니다. 다시입력해주세요.");
+					break;
+			}
+		}
 		
 	}
 }
